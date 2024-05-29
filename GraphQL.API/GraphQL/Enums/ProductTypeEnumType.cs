814592b5c35d675cf268dc50025d26beb83846fd
Mohamed <aboelkassem.me@gmail.com>
2024-05-29 10:29:46 +0300

    feat: add custom enum, scalar type

---
+++ GraphQL.API/GraphQL/Enums/ProductTypeEnumType.cs
@@ -0,0 +1,14 @@
+﻿using GraphQL.Types;
+
+namespace GraphQL.API.GraphQL.Enums
+{
+    // EnumerationGraphType is base for all scalar types like int, string
+    public class ProductTypeEnumType : EnumerationGraphType<Models.ProductType>
+    {
+        public ProductTypeEnumType() 
+        {
+            Name = "Type";
+            Description = "The type of product";
+        }
+    }
+}
--- GraphQL.API/GraphQL/Types/ProductType.cs
+++ GraphQL.API/GraphQL/Types/ProductType.cs
@@ -1,4 +1,5 @@
-﻿using GraphQL.API.Models;
+﻿using GraphQL.API.GraphQL.Enums;
+using GraphQL.API.Models;
 using GraphQL.Types;
 
 namespace GraphQL.API.GraphQL.Types
@@ -10,6 +11,13 @@ namespace GraphQL.API.GraphQL.Types
             Field(x => x.Id).Description("The Id of the product");
             Field(x => x.Name).Description("The name of the product");
             Field(x => x.Description).Description("The description of the product");
+            Field(x => x.IntroducedAt).Description("When the product was first introduced");
+            Field(x => x.PhotoFileName).Description("The Photo File Name of the product");
+            Field(x => x.Price).Description("The Price of the product");
+            Field(x => x.Rating).Description("The Rating of the product");
+            Field(x => x.Stock).Description("The Stock of the product");
+
+            Field<ProductTypeEnumType>("Type").Description("The type of product"); // Custom Enum type scalar
         }
     }
 }
