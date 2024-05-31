df6d451deb99adb66279d01e9ddbbdef5ef077ff
Mohamed <aboelkassem.me@gmail.com>
2024-05-31 19:48:45 +0300

    feat: add argument api

--- GraphQL.API/GraphQL/Queries/ProductQuery.cs
+++ GraphQL.API/GraphQL/Queries/ProductQuery.cs
@@ -12,6 +12,15 @@ namespace GraphQL.API.GraphQL.Queries
             This.Field<ListGraphType<GraphQL.Types.ProductType>>(name: "products")
                 .ResolveAsync(async context => await productRepository.GetAllAsync())
                 .Description("Get all available products");
+
+            This.Field<GraphQL.Types.ProductType>(name: "product")
+                .Argument<int>(name: "id", nullable: false) // nullable: false meaning is mandatory
+                .ResolveAsync(async context =>
+                {
+                    var id = context.GetArgument<int>("id");
+                    return await productRepository.GetAsync(id);
+                }).Description("Get specific product by id");
+
         }
     }
 }
